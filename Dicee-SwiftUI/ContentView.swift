7d7b3591d442796ac3e0b20fd99d8c0fe4b7ef18
Luis Vargas <luisvargasr10@gmail.com>
2020-12-01 15:30:15 -0600

    @State en estructuras y como modificar variables de una view

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -9,13 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var leftDiceeNumber = 1
+    @State private var rightDiceeNumber = 2
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceeView(n: leftDiceeNumber)
+                    DiceeView(n: rightDiceeNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.rightDiceeNumber = Int.random(in: 1...6)
+                    self.leftDiceeNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                }
+                .background(Color.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceeView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
